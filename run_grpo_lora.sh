083cb71d9fab6e5973c5ffcc2b6ecdf96beeaaa3
Runyu Zhou <149618011+Runyu-Zhou05@users.noreply.github.com>
2026-04-22 21:36:52 +0800

    Update run_grpo_lora.sh

--- run_grpo_lora.sh
+++ run_grpo_lora.sh
@@ -1,1 +1,1 @@
-CUDA_LAUNCH_BLOCKING=1 PYTHONUNBUFFERED=1 BATCH_SIZE_PER_DEVICE=4 MODEL_SIZE=7 accelerate launch --num-processes=4 --gpu_ids 4,5,6,9 grpo_lora.py
+CUDA_LAUNCH_BLOCKING=1 PYTHONUNBUFFERED=1 BATCH_SIZE_PER_DEVICE=4 MODEL_SIZE=7 accelerate launch --num-processes=4 --gpu_ids 0,1,2,3 grpo_lora.py
